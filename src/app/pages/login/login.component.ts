a10c7e77d7d2bd3b143673419e7ebc6c73c71eb7
roeurtke <kesey.roeurth@gmail.com>
2025-04-22 23:31:11 +0700

    Updated login form.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,16 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  handleLogin() {
-    const usernameInput = document.getElementById('username') as HTMLInputElement | null;
-    const passwordInput = document.getElementById('password') as HTMLInputElement | null;
-    
-    const username = usernameInput?.value;
-    const password = passwordInput?.value;
-    if (username && password) {
-        alert('Login successful!');
-    } else {
-        alert('Please fill in all fields.');
-    }
-}
+
 }
