695b1c97806e8d8436cd8b488bb26fedfedda214
roeurtke <kesey.roeurth@gmail.com>
2025-04-27 18:22:44 +0700

    Updated service and user interface.

--- src/app/interfaces/fetch-data.interface.ts
+++ src/app/interfaces/fetch-data.interface.ts
@@ -1,2 +1,11 @@
-export interface FetchData {
+export interface User {
+    id: number;
+    username?: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    limit_balance: number;
+    role: string;
+    created_at: string;
+    updated_at: string;
 }
