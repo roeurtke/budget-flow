abee608b0d99cad08b09cb110584106eab5e1f5d
roeurtke <kesey.roeurth@gmail.com>
2025-05-12 22:08:21 +0700

    Changed role from textbox to select box.

--- src/app/pages/users/create/create.component.ts
+++ src/app/pages/users/create/create.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  roles = [
+    { value: 'admin', label: 'Admin' },
+    { value: 'editor', label: 'Editor' },
+    { value: 'viewer', label: 'Viewer' }
+  ];
+
   constructor(private router: Router) {}
 
   onCancel(): void {
