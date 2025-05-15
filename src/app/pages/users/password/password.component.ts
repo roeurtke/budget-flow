b7e5d2b27a0b529909f2e8cf15848b6a632592ca
kroeurt <kesei.roeurt@allweb.com.kh>
2025-05-15 18:04:26 +0700

    Added change password component.

---
+++ src/app/pages/users/password/password.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password',
+  imports: [],
+  templateUrl: './password.component.html',
+  styleUrl: './password.component.css'
+})
+export class PasswordComponent {
+
+}
