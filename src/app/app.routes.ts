79958260270b5fb924904c725b1a78bff13a9070
roeurtke <kesey.roeurth@gmail.com>
2025-05-18 11:41:53 +0700

    Added module create, detail, and update for role and permission.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,11 +9,14 @@ import { IncomeCategoriesComponent } from './pages/income-categories/list/income
 import { ExpensesComponent } from './pages/expenses/list/expenses.component';
 import { ExpenseCategoriesComponent } from './pages/expense-categories/list/expense-categories.component';
 import { UsersComponent } from './pages/users/list/users.component';
-import { DetailComponent } from './pages/users/detail/detail.component';
-import { CreateComponent } from './pages/users/create/create.component';
-import { PasswordComponent } from './pages/users/password/password.component';
-import { UpdateComponent } from './pages/users/update/update.component';
+import { DetailComponent as UserDetailComponent } from './pages/users/detail/detail.component';
+import { CreateComponent as UserCreateComponent } from './pages/users/create/create.component';
+import { PasswordComponent as UserPasswordComponent } from './pages/users/password/password.component';
+import { UpdateComponent as UserUpdateComponent } from './pages/users/update/update.component';
 import { RolesComponent } from './pages/roles/list/roles.component';
+import { DetailComponent as RoleDetailComponent } from './pages/roles/detail/detail.component';
+import { CreateComponent as RoleCreateComponent } from './pages/roles/create/create.component';
+import { UpdateComponent as RoleUpdateComponent } from './pages/roles/update/update.component';
 import { PermissionsComponent } from './pages/permissions/list/permissions.component';
 import { AbilitiesComponent } from './pages/abilities/list/abilities.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -32,11 +35,14 @@ export const routes: Routes = [
           { path: 'pages/income_categories', canActivate: [authGuard], component: IncomeCategoriesComponent },
           { path: 'pages/expense_categories', canActivate: [authGuard], component: ExpenseCategoriesComponent },
           { path: 'pages/users', canActivate: [authGuard], component: UsersComponent },
-          { path: 'pages/users/detail/:id', canActivate: [authGuard], component: DetailComponent },
-          { path: 'pages/users/create', canActivate: [authGuard], component: CreateComponent },
-          { path: 'pages/users/update/:id', canActivate: [authGuard], component: UpdateComponent },
-          { path: 'pages/users/password/:id', canActivate: [authGuard], component: PasswordComponent },
+          { path: 'pages/users/create', canActivate: [authGuard], component: UserCreateComponent },
+          { path: 'pages/users/detail/:id', canActivate: [authGuard], component: UserDetailComponent },
+          { path: 'pages/users/password/:id', canActivate: [authGuard], component: UserPasswordComponent },
+          { path: 'pages/users/update/:id', canActivate: [authGuard], component: UserUpdateComponent },
           { path: 'pages/roles', canActivate: [authGuard], component: RolesComponent },
+          { path: 'pages/roles/create', canActivate: [authGuard], component: RoleCreateComponent },
+          { path: 'pages/roles/detail/:id', canActivate: [authGuard], component: RoleDetailComponent },
+          { path: 'pages/roles/update/:id', canActivate: [authGuard], component: RoleUpdateComponent },
           { path: 'pages/permissions', canActivate: [authGuard], component: PermissionsComponent },
           { path: 'pages/abilities', canActivate: [authGuard], component: AbilitiesComponent }
         ]
