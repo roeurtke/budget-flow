88a79a8b8e9d34d972b18e0717264a7f45cf61c2
roeurtke <kesey.roeurth@gmail.com>
2025-05-25 01:06:12 +0700

    Checking return message on login page.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
