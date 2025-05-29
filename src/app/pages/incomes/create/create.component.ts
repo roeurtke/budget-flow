fa3c0b03ac8dd004bc63c88dd79b0ad4aa5a7c4c
kroeurt <kesei.roeurt@allweb.com.kh>
2025-05-29 10:50:11 +0700

    Creating income.

--- src/app/pages/incomes/create/create.component.ts
+++ src/app/pages/incomes/create/create.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IncomeService } from '../../../services/income.service'
+import { IncomeCategoryService } from '../../../services/income-category.service'
 
 @Component({
   selector: 'app-income-create',
@@ -15,6 +17,8 @@ export class CreateComponent {
 
   constructor(
     private fb: FormBuilder,
+    private incomeService: IncomeService,
+    private incomeCategoryService: IncomeCategoryService,
     private router: Router ){
       this.createForm = this.fb.group({
         date: ['', Validators.required],
@@ -23,6 +27,42 @@ export class CreateComponent {
         income_amount: ['', Validators.required],
         income_category: ['', Validators.required],
       });
+  }
+
+  ngOnInit(): void {
+    this.loadIncomeCategories();
+  }
+
+  loadIncomeCategories(): void {
+    this.incomeCategoryService.getIcomeCategories().subscribe({
+      next: (response) => {
+        this.income_categories = response.map((income_category: any) => ({
+          value: income_category.id,
+          label: income_category.name
+        }));
+      },
+      error: (err) => {
+        console.error('Failed to load income_categories: ', err);
+      }
+    });
+  }
+
+  createIncomeCategory(): void {
+    if (this.createForm.invalid) {
+      this.createForm.markAllAsTouched();
+      return;
+    }
+
+    const incomeData = this.createForm.valid;
+    console.log('Income creating data: ', incomeData)
+    this.incomeService.createIncome(incomeData).subscribe({
+      next: () => {
+        this.router.navigate(['/pages/incomes']);
+      },
+      error: (err) => {
+        console.error('Failed to create income: ', err);
+      }
+    });
   }
 
   onCancel(): void{
