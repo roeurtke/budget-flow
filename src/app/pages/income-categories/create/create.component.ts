2274e6d32e4f13299b932f89df20ff66a6490460
kroeurt <kesei.roeurt@allweb.com.kh>
2025-05-29 13:59:44 +0700

    Created incom-category.

--- src/app/pages/income-categories/create/create.component.ts
+++ src/app/pages/income-categories/create/create.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, Validators, FormGroup, AbstractControl } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { IncomeCategoryService } from '../../../services/income-category.service';
 
 @Component({
   selector: 'app-income-category-create',
@@ -14,11 +15,29 @@ export class CreateComponent {
 
   constructor(
     private fb: FormBuilder,
+    private incomeCategoryService: IncomeCategoryService,
     private router: Router ){
       this.createForm = this.fb.group({
         name: ['', Validators.required],
-        description: ['', Validators.required],
+        description: [''],
       });
+  }
+
+  createIncomeCategory(): void {
+    if (this.createForm.invalid) {
+      this.createForm.markAllAsTouched();
+      return;
+    }
+
+    const incomeCategoryData = this.createForm.value;
+    this.incomeCategoryService.createIncomeCategory(incomeCategoryData).subscribe({
+      next: () => {
+        this.router.navigate(['/pages/income_categories']);
+      },
+      error: (err) => {
+        console.error('Failed to create role:', err);
+      }
+    });
   }
 
   onCancel(): void{
